5f1861a4a35af124f9ca9866f8e754a490ed47d5
Yugz29 <yannduzelier@gmail.com>
2025-09-01 09:42:36 +0200

    fix: remove name program

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 import sys
-import os
 
-for i in range(len(sys.argv)):
-    if i == 0:
-        print(os.path.basename(sys.argv[0]))
-    else:
-        print(sys.argv[i])
+for arg in sys.argv[1:]:
+    print(arg)
